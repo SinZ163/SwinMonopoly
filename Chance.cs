86bc63f6847161a798cdc60273e248f8c61ab580
Trent Monahan <github@361zn.is>
2017-09-11 21:46:06 +1000

    Initial progress

---
+++ Chance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinMonopoly
+{
+    class Chance
+    {
+        private static Chance chance;
+
+        public static Chance Instance
+        {
+            get
+            {
+                if (chance is null)
+                {
+                    chance = new Chance();
+                }
+                return chance;
+            }
+        }
+    }
+}
---
+++ ICard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinMonopoly
+{
+    interface ICard
+    {
+        string Name { get; }
+        void OnDraw(Player person);
+    }
+}
